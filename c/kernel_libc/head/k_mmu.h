fd87b744486aaa0f0fd4b918ce6c62b1e671a2c8
laenNoCode <laennocode@gmail.com>
2022-05-18 01:11:08 +0200

    first idea of a memory map entry for storage

--- c/kernel_libc/head/k_mmu.h
+++ c/kernel_libc/head/k_mmu.h
@@ -8,6 +8,15 @@ typedef struct k_memory_entry{
 	int ACPI_30;
 } k_memory_entry;
 
+typedef struct k_memory_map_entry_PMODE{
+	unsigned int v_ptr;
+	unsigned int p_ptr;
+	unsigned int size;
+	int is_free  : 1;
+	int is_exec  : 1;
+	int is_uland : 1;
+} k_memory_map_entry_PMODE;
+
 void relocate_memory_table(void* newLocation);
 
 #endif
